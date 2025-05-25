6c58355244c99e6c6f1bf09cc28a40628c838541
emilypeng2025 <emilyjwpeng@gmail.com>
2025-05-25 22:36:00 +0300

    second commit

---
+++ week3/day1/exercisexp.py
@@ -0,0 +1,20 @@
+# Exercise 1: Introduction to Data Analysis (Easy)
+
+
+# Objective: Understand the basic overview and significance of data analysis.
+# Task:
+# Write a short essay or report on the following topics:
+
+# Q1. What is data analysis?
+# answer: data analysis is a process of inspecting, cleansing, transforming, and modeling data with the goal of discovering useful information, informing conclusiongs, and supporting decision-making. 
+
+# Q2. Why is data analysis important in modern contexts?
+# Data analysis is important to people, business, and government to make decisions based on facts and numbers, identifying trends and pattern, solving problems, predicting future events, being more innovative and dicovering new knowledge. Business also use data effectively in different fields. 
+
+# List and describe three areas where data analysis is applied today.
+#In business, they use data to make marketing strategies, operate mroe efficiently and improve customer service. Companies that do things based on data can anaylised what to do and how to do things based on data they collect. 
+#In healthcare, patient data are analysed, disease are predicted and treated with more data and studies. 
+#In technology, it is used in product development, user experience optimization and innovation. For example, games also ask for feedbacks to improve game experience for players. 
+
+#exercise 2 see jupyter and file day 1 of week 3
+
